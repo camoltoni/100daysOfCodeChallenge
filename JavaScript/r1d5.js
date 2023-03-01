29d8536efb51c9e7765065664ceff68eee6da9a3
César Alejandro Moltoni <cesarmoltoni@gmail.com>
2023-03-01 15:39:56 +0000

    Change test in r1d5.js

--- JavaScript/r1d5.js
+++ JavaScript/r1d5.js
@@ -13,7 +13,7 @@ function convertHTML(str) {
 
 
 {
-  let text = convertHTML("Schindler's List");
+  let text = convertHTML("ACME & Co");
   let result = document.getElementById("result")
   result.innerHTML += `<h4>Day 5: ${text}</h4>`
   result.innerHTML += "<quote>Replace special characters with HTML entities</quote>"
