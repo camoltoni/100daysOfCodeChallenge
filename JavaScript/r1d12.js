0bfa048e46201c67cbd860ebf925b52b7a677880
César Alejandro Moltoni <cesarmoltoni@gmail.com>
2023-03-04 20:35:28 +0000

    100 days of code challenge; round 1 day 12, start. Modified index.html

--- JavaScript/r1d12.js
+++ JavaScript/r1d12.js
@@ -1,11 +1,20 @@
 function truthCheck(collection, pre) {
-  return pre;
+  return collection.reduce((truthy, obj)=> {
+    console.log(obj[pre])
+    if(typeof(obj[pre]) == "String") {
+      if(obj[pre] == "true")
+        obj[pre] = true
+      else if(obj[pre] == "false")
+        obj[pre] = false
+    }
+    console.log(obj[pre])
+    return (obj[pre]?true:false) && truthy
+  }, true)
 }
-
-truthCheck([{name: "Quincy", role: "Founder", isBot: false}, {name: "Naomi", role: "", isBot: false}, {name: "Camperbot", role: "Bot", isBot: true}], "isBot")
 
 {
   let text = truthCheck([{name: "Quincy", role: "Founder", isBot: false}, {name: "Naomi", role: "", isBot: false}, {name: "Camperbot", role: "Bot", isBot: true}], "isBot")
+  console.log(text)
   let result = document.getElementById("result")
   result.innerHTML += `<h4>Day 11: ${text}</h4>`
   result.innerHTML += "<quote>Decode a string of binary numbers into a character string</quote>"
