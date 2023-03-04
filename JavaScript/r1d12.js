2ed562e2676291447b14e848da5f62643c8f7d01
César Alejandro Moltoni <cesarmoltoni@gmail.com>
2023-03-04 20:04:11 +0000

    100 days of code challenge; round 1 day 12, start. r1d12.js added

--- JavaScript/r1d12.js
+++ JavaScript/r1d12.js
@@ -1,15 +1,11 @@
-function binaryAgent(str) {
-  return str.split(" ").reduce((sentence, elem)=>{
-    let dec = 0
-    for(let i = 0; i < elem.length; i++) {
-      dec += elem[i] * 2**(elem.length-1-i)
-    }
-    return sentence+=String.fromCharCode(dec)
-  }, "")
+function truthCheck(collection, pre) {
+  return pre;
 }
 
+truthCheck([{name: "Quincy", role: "Founder", isBot: false}, {name: "Naomi", role: "", isBot: false}, {name: "Camperbot", role: "Bot", isBot: true}], "isBot")
+
 {
-  let text = binaryAgent("01000001 01110010 01100101 01101110 00100111 01110100 00100000 01100010 01101111 01101110 01100110 01101001 01110010 01100101 01110011 00100000 01100110 01110101 01101110 00100001 00111111")
+  let text = truthCheck([{name: "Quincy", role: "Founder", isBot: false}, {name: "Naomi", role: "", isBot: false}, {name: "Camperbot", role: "Bot", isBot: true}], "isBot")
   let result = document.getElementById("result")
   result.innerHTML += `<h4>Day 11: ${text}</h4>`
   result.innerHTML += "<quote>Decode a string of binary numbers into a character string</quote>"
