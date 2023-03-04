e2745dd84188566ed5ea42df0207352e25c6770f
César Alejandro Moltoni <cesarmoltoni@gmail.com>
2023-03-04 20:40:06 +0000

    100 days of code challenge; round 1 day 12. Added a description and final cleaning

--- JavaScript/r1d12.js
+++ JavaScript/r1d12.js
@@ -1,21 +1,18 @@
 function truthCheck(collection, pre) {
   return collection.reduce((truthy, obj)=> {
-    console.log(obj[pre])
     if(typeof(obj[pre]) == "String") {
       if(obj[pre] == "true")
         obj[pre] = true
       else if(obj[pre] == "false")
         obj[pre] = false
     }
-    console.log(obj[pre])
     return (obj[pre]?true:false) && truthy
   }, true)
 }
 
 {
   let text = truthCheck([{name: "Quincy", role: "Founder", isBot: false}, {name: "Naomi", role: "", isBot: false}, {name: "Camperbot", role: "Bot", isBot: true}], "role")
-  console.log(text)
   let result = document.getElementById("result")
-  result.innerHTML += `<h4>Day 11: ${text}</h4>`
-  result.innerHTML += "<quote>Decode a string of binary numbers into a character string</quote>"
+  result.innerHTML += `<h4>Day 12: ${text}</h4>`
+  result.innerHTML += "<quote>Check if a value is in all objects of a collection with truthy values</quote>"
 }
