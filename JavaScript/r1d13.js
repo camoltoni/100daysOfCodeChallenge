7100c84dfd8c6018dc5dc0d0f6ba847b27eb6bf3
César Alejandro Moltoni <cesarmoltoni@gmail.com>
2023-03-05 14:50:47 +0000

    100 days of code challenge; round 1 day 13, main logic ended.

--- JavaScript/r1d13.js
+++ JavaScript/r1d13.js
@@ -1,11 +1,15 @@
 function addTogether() {
-  return false;
+  if(Array.from(arguments).some((elem)=>typeof elem !== "number")) return undefined
+  if(arguments.length === 2) {
+    return arguments[0] + arguments[1]
+  } else if(arguments.length === 1) return ((arg)=>addTogether(arg, arguments[0]))
 }
 
 
 
 {
-  let text = addTogether(2,3)
+  let text = addTogether(2)([3])
+  console.log(text)
   let result = document.getElementById("result")
   result.innerHTML += `<h4>Day 12: ${text}</h4>`
   result.innerHTML += "<quote>Check if a value is in all objects of a collection with truthy values</quote>"
