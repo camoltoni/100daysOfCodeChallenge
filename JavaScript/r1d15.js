b6b9d1de8dc1c596080963f05d945657f4e434f7
César Alejandro Moltoni <cesarmoltoni@gmail.com>
2023-03-07 22:40:43 +0000

    100 days of code challenge; round 1 day 15, logic ended

--- JavaScript/r1d15.js
+++ JavaScript/r1d15.js
@@ -1,17 +1,17 @@
 function orbitalPeriod(arr) {
   const GM = 398600.4418;
   const earthRadius = 6367.4447;
-  const obj = {name:"", orbitalPeriod:0.0}
-  const a = arr[0].avgAlt + earthRadius
-  obj.name = arr[0].name
-  obj.orbitalPeriod = Math.round((Math.PI * 2) * Math.sqrt((a**3)/GM))
-  console.log(obj)
-  return [obj];
+
+  return arr.map((elem)=>{
+    const a = elem.avgAlt + earthRadius
+    return {name:elem.name, orbitalPeriod:Math.round((Math.PI * 2) * Math.sqrt((a**3)/GM))}
+  });
 }
 
 {
 
-  let text = bob.getFullName()
+  let text = orbitalPeriod([{name: "iss", avgAlt: 413.6}, {name: "hubble", avgAlt: 556.7}, {name: "moon", avgAlt: 378632.553}])
+  console.table(text)
   let result = document.getElementById("result")
   result.innerHTML += `<h4>Day 14: ${text}</h4>`
   result.innerHTML += "<quote>Object manipulation</quote>"
