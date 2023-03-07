8eb4741fc6f61281da9c8287028a954b36203420
César Alejandro Moltoni <cesarmoltoni@gmail.com>
2023-03-07 21:48:52 +0000

    100 days of code challenge; round 1 day 14, object manipulation, finished

--- JavaScript/r1d14.js
+++ JavaScript/r1d14.js
@@ -9,34 +9,29 @@ const Person = function(firstAndLast) {
   }
   this.getLastName = function() {
     return firstAndLast.split(" ")[1]
-  };
+  }
   this.setFirstName = function(first) {
     let tempFirstAndLast = firstAndLast.split(" ")
     tempFirstAndLast[0] = first
-    console.table(tempFirstAndLast)
     firstAndLast = tempFirstAndLast.join(" ")
-  };
+  }
   this.setLastName = function(last) {
     let tempFirstAndLast = firstAndLast.split(" ")
     tempFirstAndLast[1] = last
     firstAndLast = tempFirstAndLast.join(" ")
-  };
-  this.setFullName(firstAndLast) {
-    let tempFirstAndLast = fistAndLast.split()
-    this.setFirstName(tempFirstAndLast(tempFirstAndLast[0]))
-    this.setLastName(tempFirstAndLast(tempFirstAndLast))
+  }
+  this.setFullName = function(firstAndLast) {
+    let tempFirstAndLast = firstAndLast.split(" ")
+    this.setFirstName(tempFirstAndLast[0])
+    this.setLastName(tempFirstAndLast[1])
   }
   return firstAndLast;
 };
 
-const bob = new Person('Bob Ross')
-console.log(bob.getLastName())
-bob.setFirstName("Haskell")
-console.log(bob.getFirstName())
-
 {
-  let text = addTogether(2)(3)
+  const bob = new Person('Bob Ross')
+  let text = bob.getFullName()
   let result = document.getElementById("result")
-  result.innerHTML += `<h4>Day 13: ${text}</h4>`
-  result.innerHTML += "<quote>Function that sums two arguments or return a function if one argument is provided</quote>"
+  result.innerHTML += `<h4>Day 14: ${text}</h4>`
+  result.innerHTML += "<quote>Object manipulation</quote>"
 }
