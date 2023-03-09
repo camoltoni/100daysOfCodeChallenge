d938e4bda2b85fc2c2147464908d0d4f02ffd2b1
César Alejandro Moltoni <cesarmoltoni@gmail.com>
2023-03-09 20:08:58 +0000

    100 days of code challenge; round 1 day 16, added description

--- JavaScript/r1d16.js
+++ JavaScript/r1d16.js
@@ -1,7 +1,7 @@
 function palindrome(str) {
   str = str.replace(/[^a-zA-Z0-9]/g, "").toLowerCase()
-  for(let i = 0; i < str.length / 2; i++) {
-    if(str[i] !== str[str.length-1-i]) return false
+  for (let i = 0; i < str.length / 2; i++) {
+    if (str[i] !== str[str.length - 1 - i]) return false
   }
   return true;
 }
@@ -10,14 +10,9 @@ function palindrome(str) {
 
 {
 
-  let text= palindrome("My age is 0, 0 si ega ym.")
-  console.log(text)
-  console.log(palindrome("0_0 (: /-\ :) 0-0"))
-  console.log(palindrome("A man, a plan, a canal. Panama"))
-  console.log(palindrome("five|\_/|four"))
-  console.log(palindrome("Dabale arroz a la zorra el abad"))
+  let text = palindrome("My age is 0, 0 si ega ym.")
 
   let result = document.getElementById("result")
-  result.innerHTML += `<h4>Day 15: ${text}</h4>`
-  result.innerHTML += "<quote>Calculate orbital period in seconds</quote>"
+  result.innerHTML += `<h4>Day 16: ${text}</h4>`
+  result.innerHTML += "<quote>Palindrome checker</quote>"
 }
