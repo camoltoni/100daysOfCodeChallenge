d6b4898603d1e2b80d90c866d0ebc40f07f7d743
César Alejandro Moltoni <cesarmoltoni@gmail.com>
2023-03-10 23:28:50 +0000

    100 days of code challenge; round 1 day 19. Ended logic.

--- JavaScript/r1d18.js
+++ JavaScript/r1d18.js
@@ -1,11 +1,11 @@
 function rot13(str) {
   const codeA = "A".charCodeAt(0)
   const codeZ = "Z".charCodeAt(0)
-  return str.split("").map((l)=>{
-    if(l.match(/[A-Z]/)){
-      let char13 = l.charCodeAt(0)+13
-      if(char13 > codeZ)
-        char13 = codeA + char13 % codeZ -1
+  return str.split("").map((l) => {
+    if (l.match(/[A-Z]/)) {
+      let char13 = l.charCodeAt(0) + 13
+      if (char13 > codeZ)
+        char13 = codeA + char13 % codeZ - 1
       l = String.fromCharCode(char13)
     }
     return l
@@ -15,7 +15,6 @@ function rot13(str) {
 {
 
   let text = rot13("SERR PBQR PNZC")
-  console.table(text)
   let result = document.getElementById("result")
   result.innerHTML += `<h4>Day 18: ${text}</h4>`
   result.innerHTML += "<quote>Decode Caesars Cipher ROT13 encrypted strings</quote>"
--- JavaScript/r1d19.js
+++ JavaScript/r1d19.js
@@ -1,20 +1,12 @@
-function rot13(str) {
-  const codeA = "A".charCodeAt(0)
-  const codeZ = "Z".charCodeAt(0)
-  return str.split("").map((l)=>{
-    if(l.match(/[A-Z]/)){
-      let char13 = l.charCodeAt(0)+13
-      if(char13 > codeZ)
-        char13 = codeA + char13 % codeZ -1
-      l = String.fromCharCode(char13)
-    }
-    return l
-  }).join("")
+function telephoneCheck(str) {
+  var regex = /^(1\s?)?(\(\d{3}\)|\d{3})[\s\-]?\d{3}[\s\-]?\d{4}$/;
+  if (str.match(regex)) return true
+  return false
 }
 
 {
 
-  let text = rot13("SERR PBQR PNZC")
+  let text = telephoneCheck("555 555 5555")
   console.table(text)
   let result = document.getElementById("result")
   result.innerHTML += `<h4>Day 18: ${text}</h4>`
