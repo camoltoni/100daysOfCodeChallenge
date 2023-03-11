bafb5d5509ca7b24ec59f4851c712af6dab6b931
César Alejandro Moltoni <cesarmoltoni@gmail.com>
2023-03-11 23:10:40 +0000

    100 days of code challenge; round 1 day 20. Solved corner cases.

--- JavaScript/r1d20.js
+++ JavaScript/r1d20.js
@@ -1,30 +1,36 @@
 function checkCashRegister(price, cash, cid) {
-  /*const amounts = [{unit:"PENNY", amount:0.01},
-                  {unit:"NICKEL", amount:0.05},
-                  {unit:"DIME", amount:0.1},
-                  {unit:"QUARTER", amount:0.25},
-                  {unit:"DOLLAR", amount:1},
-                  {unit:"FIVE", amount:5},
-                  {unit:"TEN", amount:10},
-                  {unit:"TWENTY", amount:20},
-                  {unit:"ONE HUNDRED", amount:100}];*/
-    const amounts = [["PENNY", 0.01],
-                  ["NICKEL", 0.05],
-                  ["DIME", 0.1],
-                  ["QUARTER", 0.25],
-                  ["DOLLAR", 1],
-                  ["FIVE", 5],
-                  ["TEN", 10],
-                  ["TWENTY", 20],
-                  ["ONE HUNDRED", 100]]; 
+  const responses = new Map()
+  responses["insufficient"] = {status: "INSUFFICIENT_FUNDS", change: []}
+  responses["open"] = {status: "OPEN", change: []}
+  responses["closed"] = {status: "CLOSED", change: []}
+  function sumCid() {
+    return parseFloat(cid.reduce((a, e)=>a+=e[1], 0).toFixed(2))
+  }
+  function extendCid() {
+    const amounts = [0.01, 0.05, 0.1, 0.25, 1, 5, 10, 20, 100]
+    cid = cid.map((e, i)=>[e[0], e[1], amounts[i], parseInt(e[1] / amounts[i])])
+  }
   let diff = cash - price;
-  let change = diff;
-  return change;
+  const sum = sumCid()
+  if(sum < diff) return {status: "INSUFFICIENT_FUNDS", change: []}
+  if(sum === diff) {
+    let resp = {status: "CLOSED", change: []}
+    resp.change = cid
+    return resp
+  }
+  extendCid()
+  let change = diff
+  for(let v in cid.reverse()) {
+    if(v[2] < diff && v[1] > 0) {
+      quantity = 0;
+    }
+  }
+  return change
 }
 
 {
 
-  let text = checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]);
+  let text = checkCashRegister(19.5, 19.5+335.41, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]);
   console.table(text)
   let result = document.getElementById("result")
   result.innerHTML += `<h4>Day 19: ${text}</h4>`
